b4a5a92e23d9c7ba4896b3b9ab146d28320108b9
Guilherme Estrela <guilhermestrela@gmail.com>
2025-05-06 19:36:43 -0300

    style(components): adjust button width in SectionButton
    - Increased button width from 460px to 500px.
    - Updated mobile width from 80% to 100%.

--- src/assets/components/SectionButton/index.tsx
+++ src/assets/components/SectionButton/index.tsx
@@ -7,16 +7,16 @@ function SectionButton() {
             justifycontent="center">
                 <DivFlex width="100%" height="100%"  justifycontent="space-around" alignitems="center" backgroundColor="#FFFFFF" paddingTotal="80px" flexdirectionmob="column" justifycontentmob="center" alignitemsmob="center" gap="10px" paddingTotalmob="20px 0">
                 <ContainerButton width="100%" justifycontent="space-around" alignitems="center" paddingleft="80px" paddingright="80px" flexdirectionmob="column" gapmob="30px" gap="30px">
-                    <Button justifycontent="center" alignitems="center" borderradius="15px" border="solid 3px #023D69" width="460px" height="86px" 
+                    <Button justifycontent="center" alignitems="center" borderradius="15px" border="solid 3px #023D69" width="500px" height="86px" 
                     fontsize="18px" 
-                    backgroundColor="transparent" color="#023D69" widthmob="80%"                                        onClick={function (): void {
+                    backgroundColor="transparent" color="#023D69" widthmob="100%"                                        onClick={function (): void {
                             throw new Error("Function not implemented.");
                         } }>confira as datas das próximas ações
                     </Button>
 
-                    <Button  justifycontent="center" alignitems="center" borderradius="15px" border="solid 3px #023D69" width="460px" height="86px"
+                    <Button  justifycontent="center" alignitems="center" borderradius="15px" border="solid 3px #023D69" width="500px" height="86px"
                     fontsize="18px"
-                    backgroundColor="transparent" color="#023D69" widthmob="80%" 
+                    backgroundColor="transparent" color="#023D69" widthmob="100%" 
                     onClick={function (): void {
                             throw new Error("Function not implemented.");
                         } }>acompanhe as ações no instagram
