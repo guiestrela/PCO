bbce3fd1e545a42b47875f0016822537c0419e56
Guilherme Estrela <guilhermestrela@gmail.com>
2025-05-06 19:48:28 -0300

    refactor(Footer): Improve Footer component layout
    - Restructured component using DivFlex
    - Improved social media icons placement
    - Adjusted padding and margins
    - Enhanced responsiveness for mobile
    - Cleaned up unnecessary elements

--- src/assets/components/Footer/index.tsx
+++ src/assets/components/Footer/index.tsx
@@ -10,16 +10,20 @@ import whatsapp from '../../icons/whatsappFooter.svg';
 function Footer() {
     return (
         <>
-            <DivFlex width="100%" height="200px" alignitems="center" justifycontent="center">
+            <DivFlex width="100%" alignitems="center" justifycontent="center">
                 <DivFlex width="1440px" height="100%" backgroundColor="#023D69" justifycontent="space-between" alignitems="center">
-                <DivFlexLogo paddingleft="80px" justifycontent="center" alignitems="center" paddingTotalmob="0px">
-                    <Img src={logo} widthmob="80%"/>
-                </DivFlexLogo>
-                <DivFlexLogo justifycontent="center" alignitems="center" paddingright="80px" paddingtop="70px" paddingTotalmob="0px">
-                    <Img src={instagram} />
-                    <Img src={whatsapp} />
-                    <Img src={facebook} />
-                </DivFlexLogo>
+                    <DivFlex flexdirection="column" justifycontent="start" alignitems="center" paddingleft="80px">
+                        <DivFlexLogo justifycontent="center" alignitems="center" paddingTotalmob="0px">
+                        <Img src={logo}/>
+                        </DivFlexLogo>
+                        
+                        <DivFlexLogo justifycontent="center" alignitems="center"  paddingtop="30px" paddingTotalmob="0px">
+                        <Img src={instagram} />
+                        <Img src={whatsapp} />
+                        <Img src={facebook} />
+                        </DivFlexLogo>
+                    </DivFlex>
+                
 
                 </DivFlex>
             </DivFlex>
