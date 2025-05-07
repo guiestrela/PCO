a2f6051aff843fd62ae8eefb89807a5a7c41b4d3
Guilherme Estrela <guilhermestrela@gmail.com>
2025-05-07 10:35:35 -0300

    style(Header): improve component layout
    - Added spacing for better readability.
    - Improved alignment of elements.

--- src/assets/components/Header/index.tsx
+++ src/assets/components/Header/index.tsx
@@ -12,14 +12,17 @@ function header() {
         <>
         <DivFlex width="100%" height="200px" alignitems="center" justifycontent="center">
             <DivFlex width="1440px" height="100%" backgroundColor="#FFFFFF" justifycontent="space-between" alignitems="center">
+
                 <DivFlexLogo paddingleft="80px" justifycontent="center" alignitems="center" paddingTotalmob="0px">
                     <Img src={logo} widthmob="80%"/>
                 </DivFlexLogo>
+
                 <DivFlexLogo justifycontent="center" alignitems="center" paddingright="80px" paddingtop="70px" paddingTotalmob="0px">
                     <Img src={instagram} />
                     <Img src={whatsapp} />
                     <Img src={facebook} />
                 </DivFlexLogo>
+                
             </DivFlex>
         </DivFlex>
         </>
