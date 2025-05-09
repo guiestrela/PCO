11e975f01144b001af2eec4552b0c7bb167be604
Guilherme Estrela <guilhermestrela@gmail.com>
2025-05-09 19:23:30 -0300

    style(components): update SectionButton styling
    - Adjusted mobile padding
    - Increased gap between buttons

--- src/assets/components/SectionButton/index.tsx
+++ src/assets/components/SectionButton/index.tsx
@@ -6,9 +6,9 @@ function SectionButton() {
             <DivFlex width="100%" height="100%" alignitems="center" 
             justifycontent="center">
                 
-                <DivFlex width="1440px" height="100%"  justifycontent="center" alignitems="center" backgroundColor="#FFFFFF" paddingTotal="80px" flexdirectionmob="column" justifycontentmob="center" alignitemsmob="center" gap="10px" paddingTotalmob="20px 0">
+                <DivFlex width="1440px" height="100%"  justifycontent="center" alignitems="center" backgroundColor="#FFFFFF" paddingTotal="80px" flexdirectionmob="column" justifycontentmob="center" alignitemsmob="center" gap="10px" paddingTotalmob="20px 20px">
 
-                <ContainerButton width="100%" justifycontent="space-between" alignitems="center" flexdirectionmob="column" gapmob="30px" gap="30px">
+                <ContainerButton width="100%" justifycontent="space-between" alignitems="center" flexdirectionmob="column" gapmob="30px" gap="300px">
                     <Button justifycontent="center" alignitems="center" borderradius="15px" border="solid 3px #023D69" width="100%" height="86px" 
                     fontsize="18px" 
                     backgroundColor="transparent" color="#023D69" widthmob="100%"                                        onClick={function (): void {
@@ -17,7 +17,7 @@ function SectionButton() {
                     </Button>
 
                     <Button  justifycontent="center" alignitems="center" borderradius="15px" border="solid 3px #023D69" width="100%" height="86px"
-                    fontsize="18px"
+                    fontsize="18px" 
                     backgroundColor="transparent" color="#023D69" widthmob="100%" 
                     onClick={function (): void {
                             throw new Error("Function not implemented.");
